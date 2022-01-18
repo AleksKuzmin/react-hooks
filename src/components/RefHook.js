3c046240b85bf2979388fbcc266beba1fd7a7ad4
Alex <kuzminbass@gmail.com>
2022-01-18 14:03:36 +1000

    ref

--- src/components/RefHook.js
+++ src/components/RefHook.js
@@ -1,5 +1,19 @@
+import { useRef } from "react";
+
 const RefHook = () => {
-  return <div>coming</div>;
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <h1> Alex </h1>
+      <input type="text" placeholder="Enter ..." ref={inputRef} />
+      <button onClick={onClick}> Change name</button>
+    </div>
+  );
 };
 
 export default RefHook;
