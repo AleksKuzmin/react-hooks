5d6408687356c3451d29f442b4eb9fca3efc990b
Alex <kuzminbass@gmail.com>
2022-01-18 11:42:53 +1000

    effect

--- src/components/EffectHook.js
+++ src/components/EffectHook.js
@@ -1,16 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const EffectHook = () => {
+  const [data, setData] = useState("");
+  const [count, SetCount] = useState(0);
+  useEffect(() => {
+    axios
+      .get(
+        "http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json"
+      )
+      .then((response) => {
+        setData(response.data.product);
+        console.log("api called");
+      });
+  }, []);
 
-const EffectHook =() => {
+  const countClick = () => {
+    SetCount(count + 1);
+  };
 
-
-
-
-
-return (
-
-    <div>Hello</div>
-)
-
-}
+  return (
+    <div>
+      Hello {data} {count}
+      <button onClick={countClick}>Click</button>
+    </div>
+  );
+};
 
 export default EffectHook;
